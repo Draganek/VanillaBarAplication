8ebd5684f9574f8a31ee3f34a5a05b365263ee66
Jakub Fron <jfron@jabra.com>
2022-12-23 21:21:38 +0100

    feature: add new endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const port = 6969;
+
+app.use(express.urlencoded({
+    extended: true
+}))
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -10,10 +15,25 @@ app.get('/test', (req, res) => {
     res.send('Hello Test!');
 });
 
-app.get('/login', (req, res) => {
-    const test = req.query.arg1;
-    res.send(test);
+app.get('/login', async (req, res) => {
+    const { username, password } = req.query;
+    try {
+        const usersFile = await fs.readFileSync('users.json', 'utf-8');
+        const users = JSON.parse(usersFile);
+        const user = users.find(user => user.username === username && user.password === password)
+        res.send(user ? true : false);
+    } catch (err) {
+        console.error('Error reading list of users from a file', err);
+        res.send(false);
+    }
 });
+
+app.post('/register', (req, res) => {
+    console.log(req.query);
+    console.log(req.params);
+    console.log(req.body);
+    res.send(req.body);
+})
 
 app.listen(port, () => {
     console.log(`Pierwszy serwer koxowy na porcie: ${port}`)
