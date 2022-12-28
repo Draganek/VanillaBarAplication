c0808687b21f68562292cfc775c7385320938925
Jakub Fron <jfron@jabra.com>
2022-12-28 11:15:10 +0100

    feature: implement user auth in the backend

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,67 @@
+const { json, urlencoded } = require('express');
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const fs = require('fs');
+const app = express();
 const port = 6969;
 
-app.use(express.urlencoded({
+app.use(cors());
+app.use(json());
+app.use(urlencoded({
     extended: true
-}))
+}));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/*', (_, res) => {
+    res.send('Nothing interesting here!');
 });
 
-app.get('/test', (req, res) => {
-    res.send('Hello Test!');
-});
-
-app.get('/login', async (req, res) => {
+app.get('/login', (req, res) => {
     const { username, password } = req.query;
+    if (!username || !password) {
+        res.send(false);
+        return;
+    }
     try {
-        const usersFile = await fs.readFileSync('users.json', 'utf-8');
+        const usersFile = fs.readFileSync('users.json', 'utf-8');
         const users = JSON.parse(usersFile);
         const user = users.find(user => user.username === username && user.password === password)
         res.send(user ? true : false);
     } catch (err) {
-        console.error('Error reading list of users from a file', err);
+        console.error(`Error reading list of users from a file ${err}`);
         res.send(false);
     }
 });
 
 app.post('/register', (req, res) => {
-    console.log(req.query);
-    console.log(req.params);
-    console.log(req.body);
-    res.send(req.body);
-})
+    const { username, password, price } = req.body;
+    if (!username || !password) {
+        res.send(false);
+        return;
+    }
+    try {
+        const usersFile = fs.readFileSync('users.json', 'utf-8');
+        const users = JSON.parse(usersFile);
+        const isUsernameTaken = users.find(user => user.username === username);
+        if (isUsernameTaken) {
+            res.send(false);
+            return;
+        }
+        const newUser = {
+            username,
+            password
+        };
+        if (price) {
+            newUser.price = price;
+        }
+        users.push(newUser);
+        fs.writeFileSync('users.json', JSON.stringify(users, null, 2));
+        res.send(true);
+    } catch (err) {
+        console.error(`Error writing user into the list of users file: ${err}`);
+        res.send(false);
+    }
+});
 
 app.listen(port, () => {
-    console.log(`Pierwszy serwer koxowy na porcie: ${port}`)
+    console.log(`Server running on port: ${port}`)
 });
