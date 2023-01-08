0c8b79c7ab823c18e0d5257a61601ca0dba7c86e
Radoslaw Draganek <radek.draganek@gmail.com>
2023-01-08 21:36:56 +0100

    drink list update

--- frontend/drink_list_picker.js
+++ frontend/drink_list_picker.js
@@ -13,7 +13,7 @@ async function allDrinkChoices() {
     let drink_holder = document.getElementById("drink_list_show");
     let response = await fetch(`menulist.json`);
     let obiekt = await response.json();
-    for (let element of obiekt.coctails) {
+    for (let element of obiekt.cocktails) {
         let div = document.createElement("div");
         let img = document.createElement("img");
         let name = document.createElement("a")
@@ -34,9 +34,43 @@ async function drinkChoice(drinkName, menuBar, previousView, cocktailView){
     document.getElementById(previousView).style.display='none';
     document.getElementById(cocktailView).style.display='block';
 
-    let drink_list_page = document.getElementById("drink_list_page");
-    let created_list = document.getElementById("drink_list_show");
-    drink_list_page.removeChild(created_list);
+    let cocktailShowPage = document.getElementById("cocktailShowPage");
+    
+
+    let response = await fetch(`menulist.json`);
+    let drinks = await response.json();
+    let drink = drinks.cocktails.find(cocktail => cocktail.name === drinkName);
+    
     let div = document.createElement("div");
     div.className = "drink_window";
+
+    let image = document.createElement("img");
+    image.className = "drink_img";
+    image.src = drink.link;
+    div.append(image);
+
+    let printName = document.createElement("p");
+    printName.textContent = drink.name;
+    printName.className = "drink_name"
+    div.append(printName);
+
+    let printAlcohols = document.createElement("p");
+    printAlcohols.textContent = `Alkohole: ${drink.alcohols}`;
+    printAlcohols.className = "drink_name"
+    div.append(printAlcohols);
+
+    let printfillers = document.createElement("p");
+    printfillers.textContent = `Napoje: ${drink.fillers}`;
+    printfillers.className = "drink_name"
+    div.append(printfillers);
+
+    let printPrice = document.createElement("p");
+    printPrice.textContent = `Cena: ${drink.price}zł`;
+    printPrice.className = "drink_name"
+    div.append(printPrice);
+
+    cocktailShowPage.append(div);
+
+    
+    
 }
